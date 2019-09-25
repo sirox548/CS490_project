b227bada9a7fbe307af04728681d9a97b3d519fa
alyssa099 <alyssa.mitchko@gmail.com>
2019-09-25 19:52:04 -0400

    Minor refactoring, adding status verification

--- frontend/login.js
+++ frontend/login.js
@@ -1,37 +1,42 @@
-function login(){
-    var request;
-    try{
-        request = new XMLHttpRequest();
-    }
-    catch{
-        request = new ActiveXObject("Microsoft.XMLHTTP");
-    }
+function login() {
+	var request;
+	try {
+		request = new XMLHttpRequest();
+	}
+	catch{
+		request = new ActiveXObject("Microsoft.XMLHTTP");
+	}
 
-    request.onreadystatechange = function(){
-				if (request.readyState == 4){
-					var responseData = JSON.parse(request.responseText);
-					var output = "";
-					if(responseData.database=="Can login to database"){
-						output += "<center><h2><font color='blue'>"+responseData.database+"</font></h2></center>";
-					}
-					else{
-						output += "<center><h2><font color='red'>"+responseData.database+"</font></h2></center>";
-					}
-					output += "<br>";
-					if(responseData.njit=="Can login to NJIT"){
-						output += "<center><h2><font color='blue'>"+responseData.njit+"</font></h2></center>";
-					}
-					else{
-						output += "<center><h2><font color='red'>"+responseData.njit+"</font></h2></center>";
-					}
-					document.getElementById("response").innerHTML = output;
-				}
+	request.onreadystatechange = function () {
+
+		if (request.readyState === 4 && request.status === 200) {
+			var responseData = JSON.parse(request.responseText);
+			var output = "";
+
+			if (responseData.database == "Can login to database") {
+				output += "<center><h2><font color='blue'>" + responseData.database + "</font></h2></center>";
+			}
+			else {
+				output += "<center><h2><font color='red'>" + responseData.database + "</font></h2></center>";
 			}
 
-    var user = document.getElementById("ucid").value;
-    var pswd = document.getElementById("pass").value;
-    credentials = {"username": user, "password": pswd}
+			output += "<br>";
 
-    request.open("POST", "https://web.njit.edu/~mo27/CS490/alphamiddle.php", true)
-    request.send(JSON.stringify(credentials));
+			if (responseData.njit == "Can login to NJIT") {
+				output += "<center><h2><font color='blue'>" + responseData.njit + "</font></h2></center>";
+			}
+			else {
+				output += "<center><h2><font color='red'>" + responseData.njit + "</font></h2></center>";
+			}
+
+			document.getElementById("response").innerHTML = output;
+		}
+	}
+
+	var user = document.getElementById("ucid").value;
+	var pswd = document.getElementById("pass").value;
+	credentials = { "username": user, "password": pswd }
+
+	request.open("POST", "https://web.njit.edu/~mo27/CS490/alphamiddle.php", true)
+	request.send(JSON.stringify(credentials));
 }
