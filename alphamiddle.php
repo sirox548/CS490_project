d932b2336b2292472723bf0b25f68488a1b788f5
Ryan Budhu <40608737+ryanjbudhu@users.noreply.github.com>
2019-09-25 12:34:25 -0400

    Update alphamiddle.php
    line 48: http_build_query for POSTing values
    line 49/line53: http:// –> https://

--- alphamiddle.php
+++ alphamiddle.php
@@ -45,12 +45,12 @@ function loginscript($username, $password)
 { $stringdata =  array('username'=> $username, 'password' => $password);
   $infoback = curl_init();
   curl_setopt($infoback, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($infoback, CURLOPT_POSTFIELDS, json_encode($stringdata));
-  curl_setopt($infoback, CURLOPT_URL,"http://web.njit.edu/~rjb57/CS490/validate.php");
+  curl_setopt($infoback, CURLOPT_POSTFIELDS, http_build_query($stringdata));
+  curl_setopt($infoback, CURLOPT_URL,"https://web.njit.edu/~rjb57/CS490/validate.php");
   curl_setopt($infoback, CURLOPT_COOKIEJAR, realpath($cookie));
   curl_setopt($infoback, CURLOPT_COOKIEFILE, realpath($cookie));
   curl_setopt($infoback, CURLOPT_SSL_VERIFYPEER, false);
-  curl_setopt($infoback, CURLOPT_REFERER, "http://web.njit.edu/~rjb57/CS490/validate.php");
+  curl_setopt($infoback, CURLOPT_REFERER, "https://web.njit.edu/~rjb57/CS490/validate.php");
   $stringrcvd = curl_exec($infoback);
   curl_close ($infoback);
   return $stringrcvd;
