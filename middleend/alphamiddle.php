781a8be5078ae2947cdb13834e804985f2699165
Ryan Budhu <40608737+ryanjbudhu@users.noreply.github.com>
2019-09-26 11:05:40 -0400

    Update alphamiddle.php

--- middleend/alphamiddle.php
+++ middleend/alphamiddle.php
@@ -23,14 +23,14 @@ function loginnjitscript($username, $password)
 {	$stringdata =  array("user"=> $username, "pass" => $password, "uuid" => "0xACA021");
 	$cookie = "cookie.txt";
 	$infoback = curl_init();
-	curl_setopt($infoback, CURLOPT_URL, "https://aevitepr2.njit.edu/MyHousing/login.html");
+	curl_setopt($infoback, CURLOPT_URL, "https://aevitepr2.njit.edu/MyHousing/login.cfm");
 	curl_setopt($infoback, CURLOPT_HEADER, 0);
 	curl_setopt($infoback, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($infoback, CURLOPT_POSTFIELDS, http_build_query($stringdata));
 	curl_setopt($infoback, CURLOPT_COOKIEJAR, realpath($cookie));
 	curl_setopt($infoback, CURLOPT_COOKIEFILE, realpath($cookie));
 	curl_setopt($infoback, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($infoback, CURLOPT_REFERER, "https://aevitepr2.njit.edu/MyHousing/login.html"); 
+	curl_setopt($infoback, CURLOPT_REFERER, "https://aevitepr2.njit.edu/MyHousing/login.cfm"); 
 	$stringrcvd = curl_exec($infoback);
 	curl_close ($infoback);	
 //	if (strpos($stringrcvd,"Cannot login - error")==false) return "Can login to NJIT";
