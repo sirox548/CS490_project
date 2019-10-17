0cc04f3b80ddad0b3a39504e31071a53a15bfac8
Alyssa Mitchko <alyssa.mitchko@gmail.com>
2019-10-17 16:35:22 -0400

    added post for create exam

--- frontend/posts.php
+++ frontend/posts.php
@@ -10,6 +10,8 @@
 	$output = "none";
 	$difficulty = "none";
 	$category = "none";
+	$examName = "none";
+	$examQuestions = "none";
 	
 	if ( isset($_POST['ucid'])){ $username=$_POST['ucid'];}
 	if ( isset($_POST['pwd'])){ $password=$_POST['pwd'];}    
@@ -20,6 +22,8 @@
 	if ( isset($_POST['output'])){ $output=$_POST['output'];}
 	if ( isset($_POST['difficulty'])){ $difficulty=$_POST['difficulty'];}
 	if ( isset($_POST['category'])){ $category=$_POST['category'];}
+	if ( isset($_POST['examName'])){ $difficulty=$_POST['examName'];}
+	if ( isset($_POST['examQuestions'])){ $category=$_POST['examQuestions'];}
 
 	//post type must be implemented on back and middle 
 	// if ($postType == "login"){
@@ -34,6 +38,10 @@
 	elseif ($postType == "questionBank") {
 		$stringdata =  array('postType'=> $postType);
 	}
+	elseif ($postType == "createExam") {
+		$stringdata =  array('postType'=> $postType, 'examName' => $examName, 'examQuestions' => $examQuestions);
+	}
+
 	
 	$infoback = curl_init();
 	curl_setopt($infoback, CURLOPT_RETURNTRANSFER, 1);
