a897a93112def4fe5a6f48b4a47545b77bd1057e
marianoleksy <55764282+marianoleksy@users.noreply.github.com>
2019-10-18 21:43:52 -0400

    Update betamiddlemain.php

--- middleend/betamiddlemain.php
+++ middleend/betamiddlemain.php
@@ -1,28 +1,37 @@
 <?php
+$postType=$_POST['postType'];
 $username=$_POST['ucid'];
 $password=$_POST['pwd'];
 $question=$_POST['question'];
 $funcName=$_POST['funcName'];
-$params=$_PSOT['params'];
+$params=$_POST['params'];
 $input=$_POST['input'];
 $output=$_POST['output'];
 $difficulty=$_POST['difficulty'];
 $category=$_POST['category'];
 $examName=$_POST['examName'];
 $examQuestions=$_POST['examQuestions'];
+$newQuestions=$_POST['newQuestions'];
+$paramsValues=$_POST['paramsValues'];
+$testcase=$_POST['testcase'];
+$exampoints=$_POST['exampoints'];
 
-  $stringdata =  array('username'=> $username,
-					   'password'=> $password,
-					   'question'=> $question,
-					   'funcName'=> $funcName,
-					   'params'=> $params,
-					   'input'=> $input,
-					   'output'=> $output,
-					   'difficulty'=> $difficulty,
-					   'category'=> $category,
-					   'examName'=> $examName,
-					   'examQuestions'=> $examQuestions
-					    );
+  $stringdata =  array('postType'=> $postType,
+                       'username'=> $username,
+                       'password'=> $password,
+                       'question'=> $question,
+                       'funcName'=> $funcName,
+                       'params'=> $params,
+                       'input'=> $input,
+                       'output'=> $output,
+                       'difficulty'=> $difficulty,
+                       'category'=> $category,
+                       'examName'=> $examName,
+                       'examQuestions'=> $examQuestions,
+                       'newQuestions'=> $newQuestions,
+                       'paramsValues'=> $paramsValues,
+                       'testcase'=> $testcase,
+                       'exampoints'=> $exampoints );
                        
 	$infoback = curl_init();
  	curl_setopt($infoback, CURLOPT_RETURNTRANSFER, 1);
