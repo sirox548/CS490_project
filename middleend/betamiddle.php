7434e47bf7deab553b203a20d1cad1f123080e4f
marianoleksy <55764282+marianoleksy@users.noreply.github.com>
2019-10-20 18:29:59 -0400

    Update betamiddle.php

--- middleend/betamiddle.php
+++ middleend/betamiddle.php
@@ -1,30 +1,28 @@
 <?php
-//**************CS490 Project ************
-//**************Marian Oleksy ************
 $username="none";
 $password="none";
 if ( isset($_POST['ucid']), isset($_POST['pwd']))
 	{ $username=$_POST['ucid'];
-	  $password=$_POST['pwd'];}
-//$login_to_NJIT_website = ",\"njit\":\"".loginnjitscript($username,$password)."\"}";
+	  $password=$_POST['pwd'];
+	}
 $login_to_database = loginscript($username,$password);
 $output = $login_to_database;
 echo $output;
-// function for login to database
+
 function loginscript($username, $password)
-{ $stringdata =  array('username'=> $username, 'password' => $password);
-	$infoback = curl_init();
-	curl_setopt($infoback, CURLOPT_RETURNTRANSFER, 1);
+{ $stringdata =  array('username'=> $username,
+					   'password' => $password);
+  $infoback = curl_init();
+  curl_setopt($infoback, CURLOPT_RETURNTRANSFER, 1);
   curl_setopt($infoback, CURLOPT_POST, 1);
-	curl_setopt($infoback, CURLOPT_POSTFIELDS, http_build_query($stringdata));
-	curl_setopt($infoback, CURLOPT_URL,"https://web.njit.edu/~rjb57/CS490/betabackend.php");
-//	curl_setopt($infoback, CURLOPT_COOKIEJAR, realpath($cookie));
-//	curl_setopt($infoback, CURLOPT_COOKIEFILE, realpath($cookie));
-//	curl_setopt($infoback, CURLOPT_SSL_VERIFYPEER, false);
-//	curl_setopt($infoback, CURLOPT_REFERER, "https://web.njit.edu/~rjb57/CS490/validate.php");
-	$stringrcvd = curl_exec($infoback);
-	curl_close ($infoback);
-	return $stringrcvd;
- // echo $stringrcvd;
+  curl_setopt($infoback, CURLOPT_POSTFIELDS, http_build_query($stringdata));
+  curl_setopt($infoback, CURLOPT_URL,"https://web.njit.edu/~rjb57/CS490/betabackendGIT.php");
+  curl_setopt($infoback, CURLOPT_COOKIEJAR, realpath($cookie));
+  curl_setopt($infoback, CURLOPT_COOKIEFILE, realpath($cookie));
+  curl_setopt($infoback, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($infoback, CURLOPT_REFERER, "https://web.njit.edu/~rjb57/CS490/validate.php");
+  $stringrcvd = curl_exec($infoback);
+  curl_close ($infoback);
+  return $stringrcvd;
 }
 ?>
