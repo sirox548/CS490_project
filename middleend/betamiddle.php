7576825caecf0c60c6ff96648d4044b980781489
Ryan Budhu <40608737+ryanjbudhu@users.noreply.github.com>
2019-10-22 11:37:52 -0400

    Update betamiddle.php

--- middleend/betamiddle.php
+++ middleend/betamiddle.php
@@ -1,22 +1,24 @@
 <?php
+$postType = "login";
 $username="none";
 $password="none";
-if ( isset($_POST['ucid']), isset($_POST['pwd']))
-	{ $username=$_POST['ucid'];
-	  $password=$_POST['pwd'];
+if ( $_POST['postType']=='login' )
+	{ $postType=$_POST['postType'];
+    $username=$_POST['ucid'];
+    $password=$_POST['pwd'];
 	}
-$login_to_database = loginscript($username,$password);
-$output = $login_to_database;
+$output = loginscript($username,$password);
 echo $output;
 
 function loginscript($username, $password)
-{ $stringdata =  array('username'=> $username,
-					   'password' => $password);
+{ $stringdata =  array('postType'=> $postType,
+                      'username'=> $username,
+					            'password' => $password);
   $infoback = curl_init();
   curl_setopt($infoback, CURLOPT_RETURNTRANSFER, 1);
   curl_setopt($infoback, CURLOPT_POST, 1);
   curl_setopt($infoback, CURLOPT_POSTFIELDS, http_build_query($stringdata));
-  curl_setopt($infoback, CURLOPT_URL,"https://web.njit.edu/~rjb57/CS490/betabackendGIT.php");
+  curl_setopt($infoback, CURLOPT_URL,"https://web.njit.edu/~rjb57/CS490/betabackend.php");
   curl_setopt($infoback, CURLOPT_COOKIEJAR, realpath($cookie));
   curl_setopt($infoback, CURLOPT_COOKIEFILE, realpath($cookie));
   curl_setopt($infoback, CURLOPT_SSL_VERIFYPEER, false);
